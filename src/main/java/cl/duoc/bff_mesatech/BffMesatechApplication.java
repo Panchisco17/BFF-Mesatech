87c7d3f6fe62d72b3babae409ed3dbad6c68367b
Panchisco17 <fra.olivaresp@duocuc.cl>
2026-09-22 01:27:30 -0300

    Primer commit

---
+++ src/main/java/cl/duoc/bff_mesatech/BffMesatechApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.bff_mesatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffMesatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffMesatechApplication.class, args);
+	}
+
+}
