8c0bed7d8ca2b2f0a3edad0d29e0a2c97fa4f655
Panchisco17 <fra.olivaresp@duocuc.cl>
2026-09-22 01:34:36 -0300

    creación security.config.java

---
+++ src/main/java/cl/duoc/bff_mesatech/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package cl.duoc.bff_mesatech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                // Exigir el scope Solicitudes.Read creado en Azure
+                .requestMatchers("/api/**").hasAuthority("SCOPE_Solicitudes.Read")
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        
+        return http.build();
+    }
+}
